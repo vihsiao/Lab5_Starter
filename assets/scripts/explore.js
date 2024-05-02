397240cd8a245660b7689fc739c49393eb6e956d
vihsiao <vihsiao@ucsd.edu>
2024-05-01 21:55:08 -0700

    explore.js implementation complete

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,7 +2,69 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+let selectedVoice = "";
+
 function init() {
-  // TODO
+  if (typeof speechSynthesis === "undefined") {
+    console.log("Speech synthesis undefined");
+    return;
+  }
   
+  let voices = speechSynthesis.getVoices();
+
+  // TODO
+
+  const textInput = document.getElementById("text-to-speak");
+  const voiceSelect = document.getElementById("voice-select");
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+
+  voiceSelect.onchange = (event) => {
+    selectedVoice = event.target.value;
+  }
+
+  // Make speak
+  const talk_button = document.querySelector("button");
+  const face = document.querySelector("img");
+
+  talk_button.addEventListener("click", (event) => {
+    face.src = "assets/images/smiling-open.png";
+    let utterance = new SpeechSynthesisUtterance(textInput.value);
+    const selectedOption =
+        voiceSelect.selectedOptions[0].getAttribute("data-name");
+
+      for (let i = 0; i < voices.length; i++) {
+        if (voices[i].name === selectedOption) {
+          utterance.voice = voices[i];
+          utterance.onend = () => {
+            face.src = "assets/images/smiling.png";
+            speechSynthesis.cancel();
+          }
+          speechSynthesis.speak(utterance);
+          face.src = "assets/images/smiling-open.png";
+          break;
+        }
+      }
+    
+    
+  })
+
+  if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+  ) {
+    speechSynthesis.onvoiceschanged = init;
+  }
+
 }

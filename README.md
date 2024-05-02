d92caf6d8e18b9a39474d0d73097c1f5b4a784ea
vihsiao <vihsiao@ucsd.edu>
2024-05-01 18:19:49 -0700

    expose.js complete

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 5 - Starter
+
+Victor Hsiao
+
+Lab Partner: Andrew Smithwick
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,52 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  // Dropdown
+  const dropdown = document.getElementById("horn-select");
+  const audio = document.getElementsByClassName("hidden")[0];
+  
+  dropdown.onchange = () => {
+    const hornImage = document.querySelector("img");  
+    let newImage = dropdown.value + ".svg";
+    hornImage.src = "assets/images/" + newImage;
+
+    //Set audio
+    
+    console.log(audio);
+    let newAudio = dropdown.value + ".mp3";
+    audio.src = "assets/audio/" + newAudio;
+  }
+
+  // Volume Slider
+
+  const slider = document.getElementById("volume");
+
+  slider.addEventListener("change", (event) => {
+    const slider_level = Number(event.target.value);
+    const volume_img = document.querySelector("#volume-controls img");
+    const audio = document.getElementsByClassName("hidden")[0];
+
+    if (slider_level == 0) {
+      volume_img.src = "assets/icons/volume-level-0.svg";
+    } else if (slider_level >= 1 && slider_level < 33) {
+      volume_img.src = "assets/icons/volume-level-1.svg";
+    } else if (slider_level >= 33 && slider_level < 67) {
+      volume_img.src = "assets/icons/volume-level-2.svg";
+    } else if (slider_level >= 67) {
+      volume_img.src = "assets/icons/volume-level-3.svg";
+    }
+    
+    audio.volume = slider_level / 100;
+    console.log(audio.volume)
+  })
+  
+  // Audio Button
+
+  const playButton = document.querySelector("button");
+  const jsConfetti = new JSConfetti();
+  playButton.addEventListener("click", (event) => {
+    audio.play();
+    jsConfetti.addConfetti();
+  })
 }
